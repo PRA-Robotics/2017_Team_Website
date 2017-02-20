3b576d1a5a9c1e409dbcb5e1cc4e981e96cca47d
Brooks J Rady <b.j.rady@gmail.com>
2017-02-19 19:12:46 -0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    console.log("Boo!");
+    $("#homepic > div:gt(0)").hide();
+
+    setInterval(function() {
+      $('#homepic > div:first')
+      .fadeOut(1000)
+      .next()
+      .delay(1000)
+      .fadeIn(1000)
+      .end()
+      .appendTo('#homepic');
+    }, 5000);
+});
